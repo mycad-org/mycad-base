ecba53a05719ddd949fbb3618d9828085c0a6e07
Wolfgang E. Sanyer <WolfgangESanyer@gmail.com>
2021-06-30 15:02:21 -0400

    Add passing test for Line::intersects
    This checks if a point off the line does NOT intersect
    
    Signed-off-by: Wolfgang E. Sanyer <WolfgangESanyer@gmail.com>

--- test/GeometryTests.cpp
+++ test/GeometryTests.cpp
@@ -62,4 +62,35 @@ SCENARIO( "001: Line Geometry", "[geometry][line]" ) {
             RC_ASSERT(line.intersects(p));
         }
     );
+
+    rc::prop("Any point off the line does not intersect the line",
+        [](const Line& line) {
+            float u = *rc::gen::inRange(0, 1);
+            Point p = line.atU(u);
+
+            // This is probably more complex than it needs to be, but what we're
+            // going to do is offset one or more of the (x,y,z) components of
+            // the point `p` by a random non-zero amount. This way, we know that
+            // the point is not on the line, but we've randomized how far off
+            // the line it is
+            int n = *rc::gen::inRange<int>(1, 3);
+            for (int i = 0; i < n; i++)
+            {
+                float delta = *rc::gen::nonZero<float>();
+                if (i == 0)
+                {
+                    p.x += delta;
+                }
+                else if (i == 1)
+                {
+                    p.y += delta;
+                }
+                else if (i == 2)
+                {
+                    p.z += delta;
+                }
+            }
+            RC_ASSERT_FALSE(line.intersects(p));
+        }
+    );
 }
