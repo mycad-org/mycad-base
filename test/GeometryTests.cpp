44e40e82a4374f86a637ff249a47f3f12bd3060c
Wolfgang E. Sanyer <WolfgangESanyer@gmail.com>
2021-06-30 09:44:51 -0400

    Improve formatting in tests
    Signed-off-by: Wolfgang E. Sanyer <WolfgangESanyer@gmail.com>

--- test/GeometryTests.cpp
+++ test/GeometryTests.cpp
@@ -6,6 +6,7 @@
 #include "rapidcheck/catch.h"
 
 using namespace mycad::geom;
+using EitherLineOrString = tl::expected<Line, std::string>;
 
 namespace rc{
     template<>
@@ -23,17 +24,19 @@ namespace rc{
 SCENARIO( "001: Line Geometry", "[geometry][line]" ) {
     rc::prop("A line cannot be constructed with only one point",
         [](const Point& p1) {
-            tl::expected<Line, std::string>  eLine = Line::makeLine(p1, p1);
+            EitherLineOrString eLine = Line::makeLine(p1, p1);
             RC_ASSERT_FALSE(eLine.has_value());
         }
     );
+
     rc::prop("A line is parametrized from u=0 to u=1",
         []() {
             Point p1 = *rc::gen::arbitrary<Point>();
             Point p2 = *rc::gen::distinctFrom(p1);
 
-            auto eLine = Line::makeLine(p1, p2);
+            EitherLineOrString eLine = Line::makeLine(p1, p2);
             RC_ASSERT(eLine.has_value());
+
             const Line& line = eLine.value();
             RC_ASSERT(line.atU(0) == p1);
             RC_ASSERT(line.atU(1) == p2);
