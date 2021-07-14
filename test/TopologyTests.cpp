9f06b6cbda5afbc68875220a7e643e5c3f93f02b
Wolfgang E. Sanyer <WolfgangESanyer@gmail.com>
2021-07-14 15:33:01 -0400

    Disable all but the first Topology test
    I plan to redo the API to remove tl::expected
    
    Signed-off-by: Wolfgang E. Sanyer <WolfgangESanyer@gmail.com>

--- test/TopologyTests.cpp
+++ test/TopologyTests.cpp
@@ -33,216 +33,216 @@ SCENARIO( "002: Vertex Topology", "[topology][vertex]" )
         /* verbose= */ true
     );
 
-    GIVEN("Two Vertices")
-    {
-        Topology topo;
-        Topology unsafe_topo;
+/*     GIVEN("Two Vertices") */
+/*     { */
+/*         Topology topo; */
+/*         Topology unsafe_topo; */
 
-        VertexID v1 = topo.addFreeVertex();
-        VertexID v2 = topo.addFreeVertex();
+/*         VertexID v1 = topo.addFreeVertex(); */
+/*         VertexID v2 = topo.addFreeVertex(); */
 
-        VertexID unsafe_v1 = unsafe_topo.addFreeVertex();
-        VertexID unsafe_v2 = unsafe_topo.addFreeVertex();
+/*         VertexID unsafe_v1 = unsafe_topo.addFreeVertex(); */
+/*         VertexID unsafe_v2 = unsafe_topo.addFreeVertex(); */
 
-        REQUIRE(unsafe_topo.hasVertex(unsafe_v1));
-        REQUIRE(unsafe_topo.hasVertex(unsafe_v2));
+/*         REQUIRE(unsafe_topo.hasVertex(unsafe_v1)); */
+/*         REQUIRE(unsafe_topo.hasVertex(unsafe_v2)); */
 
-        WHEN("An Edge is created between them")
-        {
-            Topology orig = topo;
-            auto eitherEdge = topo.makeEdge(v1, v2);
-            EdgeID unsafe_edge = unsafe_topo.unsafe_makeEdge(unsafe_v1, unsafe_v2);
+/*         WHEN("An Edge is created between them") */
+/*         { */
+/*             Topology orig = topo; */
+/*             auto eitherEdge = topo.makeEdge(v1, v2); */
+/*             EdgeID unsafe_edge = unsafe_topo.unsafe_makeEdge(unsafe_v1, unsafe_v2); */
 
-            REQUIRE(unsafe_topo.hasEdge(unsafe_edge));
+/*             REQUIRE(unsafe_topo.hasEdge(unsafe_edge)); */
 
-            if (not eitherEdge.has_value())
-            {
-                std::cout << eitherEdge.error() << '\n';
-            }
+/*             if (not eitherEdge.has_value()) */
+/*             { */
+/*                 std::cout << eitherEdge.error() << '\n'; */
+/*             } */
 
-            THEN("A unique ID is returned for that Edge")
-            {
-                REQUIRE(eitherEdge.has_value());
-            }
+/*             THEN("A unique ID is returned for that Edge") */
+/*             { */
+/*                 REQUIRE(eitherEdge.has_value()); */
+/*             } */
 
-            EdgeID edge = eitherEdge.value();
+/*             EdgeID edge = eitherEdge.value(); */
 
-            THEN("An adjacency exists between each Vertex and the new Edge")
-            {
-                // Either Edge ID from V₁
-                REQUIRE(
-                    topo.edgesAdjacentToVertex(v1).value()==
-                    std::vector<EdgeID>{edge}
-                );
-                REQUIRE(
-                    topo.edgesAdjacentToVertex(v2).value() ==
-                    std::vector<EdgeID>{edge}
-                );
-                REQUIRE(
-                    unsafe_topo.unsafe_edgesAdjacentToVertex(unsafe_v1) ==
-                    std::vector<EdgeID>{unsafe_edge}
-                );
+/*             THEN("An adjacency exists between each Vertex and the new Edge") */
+/*             { */
+/*                 // Either Edge ID from V₁ */
+/*                 REQUIRE( */
+/*                     topo.edgesAdjacentToVertex(v1).value()== */
+/*                     std::vector<EdgeID>{edge} */
+/*                 ); */
+/*                 REQUIRE( */
+/*                     topo.edgesAdjacentToVertex(v2).value() == */
+/*                     std::vector<EdgeID>{edge} */
+/*                 ); */
+/*                 REQUIRE( */
+/*                     unsafe_topo.unsafe_edgesAdjacentToVertex(unsafe_v1) == */
+/*                     std::vector<EdgeID>{unsafe_edge} */
+/*                 ); */
 
-                REQUIRE(
-                    unsafe_topo.unsafe_edgesAdjacentToVertex(unsafe_v2) ==
-                    std::vector<EdgeID>{unsafe_edge}
-                );
-            }
+/*                 REQUIRE( */
+/*                     unsafe_topo.unsafe_edgesAdjacentToVertex(unsafe_v2) == */
+/*                     std::vector<EdgeID>{unsafe_edge} */
+/*                 ); */
+/*             } */
 
-            THEN("Both Vertices are adjacent to the Edge")
-            {
-                REQUIRE(
-                    topo.getEdgeVertices(edge).value() ==
-                    std::pair<VertexID, VertexID>(v1, v2)
-                );
-                REQUIRE(
-                    unsafe_topo.unsafe_getEdgeVertices(unsafe_edge) ==
-                    std::pair<VertexID, VertexID>(unsafe_v1, unsafe_v2)
-                );
-            }
+/*             THEN("Both Vertices are adjacent to the Edge") */
+/*             { */
+/*                 REQUIRE( */
+/*                     topo.getEdgeVertices(edge).value() == */
+/*                     std::pair<VertexID, VertexID>(v1, v2) */
+/*                 ); */
+/*                 REQUIRE( */
+/*                     unsafe_topo.unsafe_getEdgeVertices(unsafe_edge) == */
+/*                     std::pair<VertexID, VertexID>(unsafe_v1, unsafe_v2) */
+/*                 ); */
+/*             } */
 
-            THEN("Either Vertex can be used to find the other across the Edge")
-            {
-                REQUIRE(topo.oppositeVertex(v1, edge).value() == v2);
-                REQUIRE(topo.oppositeVertex(v2, edge).value() == v1);
-                REQUIRE( unsafe_topo.unsafe_oppositeVertex(unsafe_v1, unsafe_edge) == unsafe_v2);
-                REQUIRE( unsafe_topo.unsafe_oppositeVertex(unsafe_v2, unsafe_edge) == unsafe_v1);
-            }
+/*             THEN("Either Vertex can be used to find the other across the Edge") */
+/*             { */
+/*                 REQUIRE(topo.oppositeVertex(v1, edge).value() == v2); */
+/*                 REQUIRE(topo.oppositeVertex(v2, edge).value() == v1); */
+/*                 REQUIRE( unsafe_topo.unsafe_oppositeVertex(unsafe_v1, unsafe_edge) == unsafe_v2); */
+/*                 REQUIRE( unsafe_topo.unsafe_oppositeVertex(unsafe_v2, unsafe_edge) == unsafe_v1); */
+/*             } */
 
-            WHEN("The Edge is deleted")
-            {
-                CHECK(topo.deleteEdge(edge));
+/*             WHEN("The Edge is deleted") */
+/*             { */
+/*                 CHECK(topo.deleteEdge(edge)); */
 
-                THEN("The Topology reverts to the previous state")
-                {
-                    REQUIRE(orig.similar(topo));
-                }
-            }
-        }
+/*                 THEN("The Topology reverts to the previous state") */
+/*                 { */
+/*                     REQUIRE(orig.similar(topo)); */
+/*                 } */
+/*             } */
+/*         } */
 
-        WHEN("There's already an Edge between them")
-        {
-            auto eitherEdge = topo.makeEdge(v1, v2);
+/*         WHEN("There's already an Edge between them") */
+/*         { */
+/*             auto eitherEdge = topo.makeEdge(v1, v2); */
 
-            THEN("We cannot create a second Edge")
-            {
-                CHECK_FALSE(topo.makeEdge(v1, v2).has_value());
-                REQUIRE_FALSE(topo.makeEdge(v2, v1).has_value());
-            }
-        }
-    }
+/*             THEN("We cannot create a second Edge") */
+/*             { */
+/*                 CHECK_FALSE(topo.makeEdge(v1, v2).has_value()); */
+/*                 REQUIRE_FALSE(topo.makeEdge(v2, v1).has_value()); */
+/*             } */
+/*         } */
+/*     } */
 
 }
 
-SCENARIO("003: Edge Topology", "[topology][edge]")
-{
-    GIVEN("A topology with a single Edge")
-    {
-        Topology topo;
-        VertexID v1 = topo.addFreeVertex();
-        VertexID v2 = topo.addFreeVertex();
-        EdgeID edge = topo.makeEdge(v1, v2).value();
+/* SCENARIO("003: Edge Topology", "[topology][edge]") */
+/* { */
+/*     GIVEN("A topology with a single Edge") */
+/*     { */
+/*         Topology topo; */
+/*         VertexID v1 = topo.addFreeVertex(); */
+/*         VertexID v2 = topo.addFreeVertex(); */
+/*         EdgeID edge = topo.makeEdge(v1, v2).value(); */
 
-        WHEN("A second Edge is added adjacent to v1")
-        {
-            VertexID v3 = topo.addFreeVertex();
-            EdgeID edge2 = topo.makeEdge(v2, v3).value();
-            THEN("v2 is adjacent to both edges")
-            {
-                auto eitherEdges = topo.edgesAdjacentToVertex(v2);
-                REQUIRE(eitherEdges.has_value());
-                REQUIRE(std::ranges::count(eitherEdges.value(), edge) == 1);
-                REQUIRE(std::ranges::count(eitherEdges.value(), edge2) == 1);
-            }
+/*         WHEN("A second Edge is added adjacent to v1") */
+/*         { */
+/*             VertexID v3 = topo.addFreeVertex(); */
+/*             EdgeID edge2 = topo.makeEdge(v2, v3).value(); */
+/*             THEN("v2 is adjacent to both edges") */
+/*             { */
+/*                 auto eitherEdges = topo.edgesAdjacentToVertex(v2); */
+/*                 REQUIRE(eitherEdges.has_value()); */
+/*                 REQUIRE(std::ranges::count(eitherEdges.value(), edge) == 1); */
+/*                 REQUIRE(std::ranges::count(eitherEdges.value(), edge2) == 1); */
+/*             } */
 
-            WHEN("A Chain is made between both edges")
-            {
-                auto eitherChain = topo.makeChain(edge, edge2);
-                REQUIRE(eitherChain.has_value());
+/*             WHEN("A Chain is made between both edges") */
+/*             { */
+/*                 auto eitherChain = topo.makeChain(edge, edge2); */
+/*                 REQUIRE(eitherChain.has_value()); */
 
-                THEN("We can recover both Edges in order using the returned Chain")
-                {
-                    Chain chain = eitherChain.value();
-                    REQUIRE(
-                        topo.getChainEdges(chain).value() ==
-                        std::vector<EdgeID>{edge, edge2});
-                }
-            }
+/*                 THEN("We can recover both Edges in order using the returned Chain") */
+/*                 { */
+/*                     Chain chain = eitherChain.value(); */
+/*                     REQUIRE( */
+/*                         topo.getChainEdges(chain).value() == */
+/*                         std::vector<EdgeID>{edge, edge2}); */
+/*                 } */
+/*             } */
 
-            WHEN("The second Edge is deleted before making a Chain")
-            {
-                topo.deleteEdge(edge2);
-                THEN("We get an error")
-                {
-                    REQUIRE_FALSE(topo.makeChain(edge, edge2).has_value());
-                }
-            }
-        }
+/*             WHEN("The second Edge is deleted before making a Chain") */
+/*             { */
+/*                 topo.deleteEdge(edge2); */
+/*                 THEN("We get an error") */
+/*                 { */
+/*                     REQUIRE_FALSE(topo.makeChain(edge, edge2).has_value()); */
+/*                 } */
+/*             } */
+/*         } */
 
-        WHEN("A second Edge is added with zero adjacencies to the first")
-        {
-            VertexID v3 = topo.addFreeVertex();
-            VertexID v4 = topo.addFreeVertex();
-            auto eitherEdge2 = topo.makeEdge(v3, v4);
+/*         WHEN("A second Edge is added with zero adjacencies to the first") */
+/*         { */
+/*             VertexID v3 = topo.addFreeVertex(); */
+/*             VertexID v4 = topo.addFreeVertex(); */
+/*             auto eitherEdge2 = topo.makeEdge(v3, v4); */
 
-            THEN("Trying to make a chain between them results in an error")
-            {
-                REQUIRE_FALSE(topo.makeChain(edge, eitherEdge2.value()).has_value());
-            }
-        }
-    }
-}
+/*             THEN("Trying to make a chain between them results in an error") */
+/*             { */
+/*                 REQUIRE_FALSE(topo.makeChain(edge, eitherEdge2.value()).has_value()); */
+/*             } */
+/*         } */
+/*     } */
+/* } */
 
-SCENARIO("004: Chain Topology", "[topology][chain]")
-{
-    GIVEN("A series of three Edges")
-    {
-        Topology topo;
-        auto v1 = topo.addFreeVertex();
-        auto v2 = topo.addFreeVertex();
-        auto v3 = topo.addFreeVertex();
-        auto v4 = topo.addFreeVertex();
-        // unsafe_makeEdge here is "ok" since we **know** that there four
-        // vertices exist in the topology. generally though, avoid this.
-        auto e1 = topo.unsafe_makeEdge(v1, v2);
-        auto e2 = topo.unsafe_makeEdge(v2, v3);
-        auto e3 = topo.unsafe_makeEdge(v3, v4);
+/* SCENARIO("004: Chain Topology", "[topology][chain]") */
+/* { */
+/*     GIVEN("A series of three Edges") */
+/*     { */
+/*         Topology topo; */
+/*         auto v1 = topo.addFreeVertex(); */
+/*         auto v2 = topo.addFreeVertex(); */
+/*         auto v3 = topo.addFreeVertex(); */
+/*         auto v4 = topo.addFreeVertex(); */
+/*         // unsafe_makeEdge here is "ok" since we **know** that there four */
+/*         // vertices exist in the topology. generally though, avoid this. */
+/*         auto e1 = topo.unsafe_makeEdge(v1, v2); */
+/*         auto e2 = topo.unsafe_makeEdge(v2, v3); */
+/*         auto e3 = topo.unsafe_makeEdge(v3, v4); */
 
-        WHEN("The first two are connected")
-        {
-            auto eitherChain = topo.makeChain(e1, e2);
-            REQUIRE(eitherChain.has_value());
-            Chain chain = *eitherChain;
+/*         WHEN("The first two are connected") */
+/*         { */
+/*             auto eitherChain = topo.makeChain(e1, e2); */
+/*             REQUIRE(eitherChain.has_value()); */
+/*             Chain chain = *eitherChain; */
 
-            THEN("The third is not part of the chain")
-            {
-                auto eitherEdges = topo.getChainEdges({v1, e1});
+/*             THEN("The third is not part of the chain") */
+/*             { */
+/*                 auto eitherEdges = topo.getChainEdges({v1, e1}); */
 
-                REQUIRE(eitherEdges.has_value());
-                REQUIRE(std::ranges::count(eitherEdges.value(), e3) == 0);
-            }
+/*                 REQUIRE(eitherEdges.has_value()); */
+/*                 REQUIRE(std::ranges::count(eitherEdges.value(), e3) == 0); */
+/*             } */
 
-            WHEN("The second two are connected")
-            {
-                topo.makeChain(e2, e3);
+/*             WHEN("The second two are connected") */
+/*             { */
+/*                 topo.makeChain(e2, e3); */
 
-                THEN("The third is still not part of the original chain")
-                {
-                    auto eitherEdges = topo.getChainEdges(chain);
+/*                 THEN("The third is still not part of the original chain") */
+/*                 { */
+/*                     auto eitherEdges = topo.getChainEdges(chain); */
 
-                    REQUIRE(eitherEdges.has_value());
-                    REQUIRE(std::ranges::count(eitherEdges.value(), e3) == 0);
-                }
+/*                     REQUIRE(eitherEdges.has_value()); */
+/*                     REQUIRE(std::ranges::count(eitherEdges.value(), e3) == 0); */
+/*                 } */
 
-                THEN("The third can be retrieved using v2 and e2")
-                {
-                    auto eitherEdges = topo.getChainEdges({v2, e2});
+/*                 THEN("The third can be retrieved using v2 and e2") */
+/*                 { */
+/*                     auto eitherEdges = topo.getChainEdges({v2, e2}); */
 
-                    REQUIRE(eitherEdges.has_value());
-                    REQUIRE(eitherEdges.value() ==
-                            std::vector<EdgeID>{e2, e3});
-                }
-            }
-        }
-    }
-}
+/*                     REQUIRE(eitherEdges.has_value()); */
+/*                     REQUIRE(eitherEdges.value() == */
+/*                             std::vector<EdgeID>{e2, e3}); */
+/*                 } */
+/*             } */
+/*         } */
+/*     } */
+/* } */
