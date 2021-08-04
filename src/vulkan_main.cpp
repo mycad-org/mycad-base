47a938bc143569169ce0a068ad6a052853a04a54
Wolfgang E. Sanyer <WolfgangESanyer@gmail.com>
2021-08-04 09:24:12 -0400

    mycad-vk: Clean up a designated initializer
    Signed-off-by: Wolfgang E. Sanyer <WolfgangESanyer@gmail.com>

--- src/vulkan_main.cpp
+++ src/vulkan_main.cpp
@@ -41,8 +41,6 @@ int main()
         // initialize the vk::InstanceCreateInfo
         vk::InstanceCreateInfo instanceCreateInfo{
             .pApplicationInfo        = &applicationInfo,
-            .enabledLayerCount       = 0,
-            .ppEnabledLayerNames     = nullptr,
             .enabledExtensionCount   = glfwExtensionCount,
             .ppEnabledExtensionNames = glfwExtensions
         };
