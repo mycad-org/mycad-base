24ec031743a92bb38cbe123a894d089088db4a06
Wolfgang E. Sanyer <WolfgangESanyer@gmail.com>
2021-08-25 23:12:13 -0400

    mycad-vk: support rendering a mycad::Entity directly
    Signed-off-by: Wolfgang E. Sanyer <WolfgangESanyer@gmail.com>

--- src/vulkan_main.cpp
+++ src/vulkan_main.cpp
@@ -5,68 +5,44 @@ const int MAX_FRAMES_IN_FLIGHT = 2;
 
 int main()
 {
-    // TODO: don't hard-code this
-    Mesh squares{{
-        {{-0.5f, -0.5f, 0.0f}, {1.0f, 0.0f, 0.0f}, {1.0f, 0.0f}},
-        {{0.5f, -0.5f, 0.0f}, {0.0f, 1.0f, 0.0f}, {0.0f, 0.0f}},
-        {{0.5f, 0.5f, 0.0f}, {0.0f, 0.0f, 1.0f}, {0.0f, 1.0f}}
-    }};
-    squares.addFragments({
-        {
-            {{0.5f, 0.5f, 0.0f}, {0.0f, 0.0f, 1.0f}, {0.0f, 1.0f}},
-            {{-0.5f, 0.5f, 0.0f}, {1.0f, 1.0f, 1.0f}, {1.0f, 1.0f}},
-            {{-0.5f, -0.5f, 0.0f}, {1.0f, 0.0f, 0.0f}, {1.0f, 0.0f}}
-        },
-        // add second square
-        {
-            {{-0.5f, -0.5f, -0.5f}, {1.0f, 0.0f, 0.0f}, {1.0f, 0.0f}},
-            {{0.5f, -0.5f, -0.5f}, {0.0f, 1.0f, 0.0f}, {0.0f, 0.0f}},
-            {{0.5f, 0.5f, -0.5f}, {0.0f, 0.0f, 1.0f}, {0.0f, 1.0f}},
-        },
-        {
-            {{0.5f, 0.5f, -0.5f}, {0.0f, 0.0f, 1.0f}, {0.0f, 1.0f}},
-            {{-0.5f, 0.5f, -0.5f}, {1.0f, 1.0f, 1.0f}, {1.0f, 1.0f}},
-            {{-0.5f, -0.5f, -0.5f}, {1.0f, 0.0f, 0.0f}, {1.0f, 0.0f}}
-        }
-    });
+    mycad::Entity ent;
+    auto v0 = ent.addVertex({0, 0, 0});
+    auto v1 = ent.addVertex({0.5, 0.5, 0});
+    auto v2 = ent.addVertex({0.0, 0.5, 0});
+    auto v3 = ent.addVertex({-0.5, -0.5, 0});
 
-    LineMesh lines{
-        {-0.5f, -0.5f, 0.0f},
-        { 0.5f, -0.5f, 0.0f}
+    ent.addEdge(v0, v1);
+    ent.addEdge(v1, v2);
+    ent.addEdge(v2, v3);
+    ent.addEdge(v3, v1);
+
+    auto edges = ent.getEdges();
+    auto getStartFinish = [](mycad::Line const & line) -> std::pair<glm::vec3, glm::vec3>
+    {
+        auto p0 = line.atU(0);
+        auto p1 = line.atU(1);
+
+        return {{p0.x, p0.y, p0.z}, {p1.x, p1.y, p1.z}};
     };
-    lines.addSegment(
-        {0.5f, -0.5f, 0.0f},
-        {0.5f, 0.5f, 0.0f}
-    );
-    lines.addSegment(
-        {0.5f, 0.5f, 0.0f},
-        {-0.5f, 0.5f, 0.0f}
-    );
-    lines.addSegment(
-        {-0.5f, 0.5f, 0.0f},
-        {-0.5f, -0.5f, 0.0f}
-    );
-    lines.addSegment(
-        {-0.5f, -0.5f, -0.5f},
-        { 0.5f, -0.5f, -0.5f}
-    );
-    lines.addSegment(
-        {0.5f, -0.5f, -0.5f},
-        {0.5f, 0.5f, -0.5f}
-    );
-    lines.addSegment(
-        {0.5f, 0.5f, -0.5f},
-        {-0.5f, 0.5f, -0.5f}
-    );
-    lines.addSegment(
-        {-0.5f, 0.5f, -0.5f},
-        {-0.5f, -0.5f, -0.5f}
-    );
+
+    auto [left, right] = getStartFinish(edges.front());
+
+    LineMesh lines{left, right};
+
+    for(auto const & edge : edges)
+    {
+        if (edge == edges.front())
+        {
+            continue;
+        }
+
+        auto [left, right] = getStartFinish(edge);
+        lines.addSegment(left, right);
+    }
 
     ApplicationData app;
 
     Renderer renderer(app.window, MAX_FRAMES_IN_FLIGHT);
-    renderer.addMesh(squares);
     renderer.addLineMesh(lines);
 
     int currentFrame = 0;
